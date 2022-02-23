4f200f40aa15e95ac386e1c0f2d264e700bbee3a
manas.deora <manas.deora@telekom-digital.com>
2022-02-24 02:33:10 +0530

    JumpGame.java unoptimized

---
+++ src/LeetcodeAlgorithms/JumpGame.java
@@ -0,0 +1,56 @@
+package LeetcodeAlgorithms;
+
+import java.util.Arrays;
+
+public class JumpGame {
+    public static void main(String[] args) {
+//        int[] nums = {2, 0, 0};
+//        int[] nums = {2, 5, 0, 0};
+//        int[] nums = {3, 2, 1, 0, 4};
+//        int[] nums = {2, 3, 1, 1, 4};
+//        int[] nums = {3, 0, 8, 2, 0, 0, 1};
+//        int[] nums = {1, 2, 3};
+        int nums[] = {2, 1, 2, 1, 2, 1};
+
+        boolean ans = canJump1(nums);
+        System.out.println(ans);
+    }
+
+
+    //Unoptimized
+    public static boolean canJump1(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        boolean[] dp = new boolean[nums.length];
+        Arrays.fill(dp, false);
+        for (int i = 0; i < nums.length; i++) {
+            if (dp[dp.length - 1] == true) {
+                break;
+            }
+            int currIndexVal = nums[i];
+            if (currIndexVal == 0) {
+                int flag = 0;
+                for (int j = i + 1; j < dp.length; j++) {
+                    if (dp[j] == true) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 1) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            if (i + currIndexVal >= nums.length - 1) {
+                dp[dp.length - 1] = true;
+                break;
+            } else {
+                dp[i + currIndexVal] = true;
+            }
+        }
+        return dp[dp.length - 1];
+    }
+}
+
